6c74578857ea94d116160bef6157e08a2c96d050
RitvarsZ <ritvarszvejnieks@protonmail.com>
2023-02-25 14:14:26 +0200

    Working multithreaded cleint server chatting.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,6 @@
 mod connect;
 
-use std::io::Write;
+use std::io::{Write, Read};
 
 use connect::*;
 
@@ -12,7 +12,45 @@ fn main() -> Result<(), ()> {
 
     println!("Connected!");
 
-    socket.write("Hello?".as_bytes()).unwrap();
+    {
+        let mut buffer = [0; 8192];
+        let mut socket = socket.try_clone().unwrap();
+        std::thread::spawn(move || {
+            println!("Thread id: {:?}, started a new thread. listening:", std::thread::current().id());
+            loop {
+                buffer.fill(0);
+                match socket.read(&mut buffer) {
+                    Ok(0) => {
+                        println!("Server disconnected");
+                        break;
+                    },
+                    Ok(_) => {
+                        println!("Received: {}", String::from_utf8_lossy(&buffer).trim());
+                    },
+                    Err(e) => {
+                        println!("Failed to read from connection: {}", e);
+                        break;
+                    },
+                }
+            }
+        });
+    }
 
+    let mut input = String::new();
+    loop {
+        input.clear();
+        std::io::stdin().read_line(&mut input).expect("Could not read line");
+        input = input.trim().to_string();
+        
+        match socket.write(input.as_bytes()) {
+            Err(_) => {
+                println!("Could not send message");
+                break;
+            },
+            _ => (),
+        }
+    }
+
+    println!("Disconnected");
     Ok(())
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,14 +1,16 @@
-use std::{net::{TcpListener, TcpStream}, io::Read};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, sync::{Mutex, Arc}};
 
 #[derive(Debug)]
 pub struct Server {
     listener: TcpListener,
+    clients: Arc<Mutex<Vec<TcpStream>>>,
 }
 
 impl Server {
     pub fn new(address: String) -> Self {
         Server {
-            listener: TcpListener::bind(address).unwrap()
+            listener: TcpListener::bind(address).unwrap(),
+            clients: Arc::new(Mutex::new(Vec::new())),
         }
     }
 
@@ -17,21 +19,42 @@ impl Server {
         
         for stream in self.listener.incoming() {
             match stream {
-                Ok(s) => self.handle_client(s),
+                Ok(s) => {
+                    self.clients.lock().unwrap().push(s.try_clone().unwrap());
+                    let clients = self.clients.clone();
+                    std::thread::spawn(move || {
+                        handle_client(s, clients);
+                    });
+                }
                 Err(e) => panic!("encountered IO error: {e}"),
             }
         }
     }
+}
 
-    fn handle_client(&self, mut stream: TcpStream) {
-        println!("New client connected: {}", stream.peer_addr().unwrap());
+fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<TcpStream>>>) {
+    println!("New client connected: {}", stream.peer_addr().unwrap());
+    println!("Thread id: {:?}", std::thread::current().id());
 
-        let mut buffer = [0; 1024];
+    let mut buffer = [0; 8192];
+    loop {
+        buffer.fill(0);
         match stream.read(&mut buffer) {
-            Ok(_) => {
-                println!("Received: {}", String::from_utf8_lossy(&buffer));
+            Ok(0) => {
+                println!("Client {} disconnected", stream.peer_addr().unwrap());
+                break;
             },
-            Err(e) => println!("Failed to read from connection: {}", e),
+            Ok(_) => {
+                println!("Received: {}", String::from_utf8_lossy(&buffer).trim());
+                clients.lock().unwrap().iter_mut().for_each(|client| {
+                    println!("Sending to: {}", client.peer_addr().unwrap());
+                    client.write(&buffer).unwrap();
+                });
+            },
+            Err(e) => {
+                println!("Failed to read from connection: {}", e);
+                break;
+            },
         }
     }
 }
